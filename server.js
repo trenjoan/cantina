f80d768394f2e325b5609d6f4f4fd7cfaf98c496
trenjoan <88joanferran.ibanez@institutaltpenedes.cat>
2025-08-05 19:46:21 +0200

    new code desde cero

--- server.js
+++ server.js
@@ -1,51 +1,9 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-
-const PORT = process.env.PORT||3000;
-
-const server = http.createServer((req, res) => {
-  const filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
-  const ext = path.extname(filePath);
-
-  // Definir el tipus de contingut segons l'extensió
-  let contentType = 'text/html';
-  switch (ext) {
-    case '.js':
-      contentType = 'text/javascript';
-      break;
-    case '.css':
-      contentType = 'text/css';
-      break;
-    case '.json':
-      contentType = 'application/json';
-      break;
-    case '.png':
-      contentType = 'image/png';
-      break;
-    case '.jpg':
-    case '.jpeg':
-      contentType = 'image/jpeg';
-      break;
-  }
-
-  // Llegir el fitxer
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end('<h1>404 Not Found</h1>', 'utf-8');
-      } else {
-        res.writeHead(500);
-        res.end(`Error: ${err.code}`);
-      }
-    } else {
-      res.writeHead(200, { 'Content-Type': contentType });
-      res.end(content, 'utf-8');
-    }
-  });
-});
-
-server.listen(PORT, () => {
-  console.log(`Servidor en marxa a http://localhost:${PORT}`);
-});
+import { readFile } from 'node:fs'
+import http from 'node:http'
+const server = http.createServer((req ,res)
+if(req.url == "/init"){readFile('/src/init.html', (err , data)=>{if(err){res.statusCode(500)
+}else{
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    res.statusCode(200)
+    res.end(data)
+}})}else if(req.url))
