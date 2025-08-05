9be8e276389e7d89e45b53b1280ed1847890b393
trenjoan <88joanferran.ibanez@institutaltpenedes.cat>
2025-08-05 20:27:04 +0200

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,10 +1,11 @@
-import { readFile } from 'node:fs'
-import http from 'node:http'
-const server = http.createServer((req ,res)
-if (req.url == "/init"){readFile('/src/init.html', (err , data)=>{
+import {createServer} from 'node:http'
+import {readFile}from 'node:fs'
+const server = createServer(if(req.url == "/init"){readFile('/src/init.html', (err , data)=>{
     if(err){res.statusCode(500)
 }else{
     res.setHeader('Content-Type', 'text/html; charset=utf-8')
     res.statusCode(200)
     res.end(data)
-}})}else(req.url){res.statusCode(404)})
+}})}else if(req.url){res.statusCode(404)})
+   
+ server.listen(3000)
