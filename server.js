47fc567c81928eeaec12efd10714e8a5a758917c
trenjoan <88joanferran.ibanez@institutaltpenedes.cat>
2025-08-05 20:31:22 +0200

    req i res

---
+++ server.js
@@ -0,0 +1,11 @@
+import {createServer} from 'node:http'
+import {readFile}from 'node:fs'
+const server = createServer((req, res)=>{if(req.url == "/init"){readFile('/src/init.html', (err , data)=>{
+    if(err){res.statusCode(500)
+}else{
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    res.statusCode(200)
+    res.end(data)
+}})}else if(req.url){res.statusCode(404)}})
+   
+ server.listen(3000)
